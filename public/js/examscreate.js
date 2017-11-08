0a9a75e0c2c224e3a0c992ab229c2811913a28a5
Carla Ramirez <carla.ramirez.rojas@gmail.com>
2017-11-07 22:39:05 -0400

    Vista Exam Create
    arreglo fn store

--- public/js/examscreate.js
+++ public/js/examscreate.js
@@ -12,30 +12,55 @@
       this.typeQuestion();
       this.addQuestion();
       this.fnChosen();
+      this.getCourses();
+      this.sendExam();
+    },
+    getCourses:function(){
+      var courses = this.consult("listcourses",{},"GET",false),data,r=$("#course");
+      courses.done(function(d){
+        for (var i in d) {
+          r.append($("<option>",{"value":d[i].id}).text(d[i].name));
+        }
+      });
+      return r;
+    },
+    fnDatepicker:function(){
+        $('.datepicker').datepicker({
+          format: 'dd/mm/yyyy',
+          startDate: '-3d'
+        });
+    },
+    fnClockpicker:function(){
+      $('.clockpicker').clockpicker({donetext:"Seleccionar",twelvehour:true,default:'now'});
     },
     createForm:function(){
-      var select = $("<select>",{"id":"courses","class":"form-control chosen-select"}),
-      row=$("<div>",{"class":"rowquestion"}).append($("<div>",{"class":"row"}).append(
+      var row="";
+      this.row=$("<div>",{"class":"rowquestion form-group"}).append($("<label>",{"class":""}).append($("<div>",{"class":"question-number"})).text("Pregunta"),[$("<div>",{"class":"row"}).append(
           $("<div>",{"class":"col-xs-8"}).append(
-            $("<input>",{"type":"text","class":"description form-control","placeholder":"Question"}).val("")
+              $("<input>",{"name":"question[]","type":"text","class":"description form-control","placeholder":"Question"}).val("")
           ),
           [
             $("<div>",{"class":"col-xs-2"}).append(
-              $("<select>",{"class":"select-question form-control"}).append(
+              $("<select>",{"name":"type[]","class":"select-question form-control"}).append(
                 $("<option>",{'value':'off'}).text(this.textQuestionSimple),[
                   $("<option>",{'value':'on'}).text(this.textQuestionSelection)
                 ]
               )
             ),
             $("<div>",{"class":"col-xs-2"}).append(
-              $("<button>",{"class":"btn btn-success addQuestion"}).text("+"),[
-                $("<button>",{"class":"btn btn-danger deleteQuestion"}).text("-")
+              $("<button>",{"class":"btn btn-success addQuestion col-xs-12"}).append($("<span>",{'class':'glyphicon glyphicon-plus-sign'})),[
+                $("<button>",{"class":"btn btn-danger deleteQuestion col-xs-12"}).append($("<span>",{"class":"glyphicon glyphicon-remove-sign"}))
               ]
             )
           ])
-      )
-      this.row=row.clone();
-      $(".panel-body").append(select.add(this.row));
+      ])
+      row=this.row.clone();
+      row.find(".deleteQuestion").remove();
+      $(".data-render").append(row);
+      this.getCourses();
+      this.fnClockpicker();
+      this.fnDatepicker();
+      $("#courses").trigger('chosen:updated');
     },
     typeQuestion:function(){
       $(".select-question").off("change");
@@ -43,17 +68,17 @@
         e.preventDefault(),e.stopPropagation();
         if($(this).val()==="on"){
           $(this).parents().eq(2).append(
+            $("<div>",{"class":"row option-title"}).append($("<div>",{"class":"col-xs-12"}).append($("<label>",{"class":""}).text("Opciones"))),[
             $("<div>",{"class":"row option-row"}).append(
-              $("<div>",{"class":"col-xs-8"}).append(
-                $("<input>",{"class":"form-control","type":"text","placeholder":"Option"})
-              ),[
-                $("<div>",{"class":"col-xs-4"}).append(
-                  $("<button>",{"class":"btn btn-success addOption"}).text("+"),[
-                    $("<button>",{"class":"btn btn-danger removeOption"}).text("-")
+              $("<div>",{"class":"col-xs-10"}).append(
+                  $("<input>",{"name":"option[]","class":"form-control","type":"text","placeholder":"Option"})
+                ),
+                $("<div>",{"class":"col-xs-2"}).append(
+                  $("<button>",{"class":"btn btn-success addOption col-xs-6"}).append($("<span>",{'class':'glyphicon glyphicon-plus-sign'})),[
+                    $("<button>",{"class":"btn btn-danger removeOption col-xs-6"}).append($("<span>",{"class":"glyphicon glyphicon-remove-sign"}))
                   ]
                 )
-              ]
-            )
+            )]
           )
           courses.addOption();
           courses.deleteOption();
@@ -68,12 +93,12 @@
         e.preventDefault(),e.stopPropagation();
         $(this).parents().eq(2).append(
           $("<div>",{"class":"row option-row"}).append(
-          $("<div>",{"class":"col-xs-8"}).append(
-            $("<input>",{"class":"form-control","type":"text","placeholder":"Option"})
+          $("<div>",{"class":"col-xs-10"}).append(
+            $("<input>",{"name":"option[]","class":"form-control","type":"text","placeholder":"Option"})
           ),[
-            $("<div>",{"class":"col-xs-4"}).append(
-              $("<button>",{"class":"btn btn-success addOption"}).text("+"),[
-                $("<button>",{"class":"btn btn-danger removeOption"}).text("-")
+            $("<div>",{"class":"col-xs-2"}).append(
+              $("<button>",{"class":"btn btn-success addOption col-xs-6"}).append($("<span>",{'class':'glyphicon glyphicon-plus-sign'})),[
+                $("<button>",{"class":"btn btn-danger removeOption col-xs-6"}).append($("<span>",{"class":"glyphicon glyphicon-remove-sign"}))
               ]
             )
           ]
@@ -90,16 +115,25 @@
         if($(this).parents('.rowquestion').find('.option-row').length>1){
           $(this).parents().eq(1).remove();
         }
-      }); 
+      });
     },
     addQuestion:function(){
       $(".addQuestion").off();
       $(".addQuestion").on("click",function(e){
         e.preventDefault(),e.stopPropagation();
-        console.log(courses.row.get(0));
-        $(this).parents().eq(3).append(courses.row.clone());
+        var x = courses.row.clone();
+        x.find(".addQuestion").remove();
+        $(this).parents().eq(3).append(x);
         courses.typeQuestion();
+        courses.removeQuestion();
         courses.addOption();
+      });
+    },
+    removeQuestion:function(){
+      $(".deleteQuestion").off();
+      $(".deleteQuestion").on("click",function(e){
+        e.preventDefault(),e.stopPropagation();
+        $(this).parents(".rowquestion").remove();
       });
     },
     fnChosen:function(){
@@ -122,7 +156,42 @@
           headers: (method=="POST"?{'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}:"")
         });
       }
-    }
+    },sendExam:function(){
+      $("#send-exam").on('click',function(){
+        var course=$("#course").val(), 
+        type=$("#type-exam").val(),
+        f_inicio=$("#fecha_inicio").val(),
+        h_inicio=$("#hora_inicio").val(),
+        f_fin=$("#fecha_fin").val(),
+        h_fin=$("#hora_final").val(),
+        preguntas = [],opciones=[],tipos=[]
+        $('input[name="question[]"]').each(function(){
+          preguntas+=(preguntas.length<=0?$(this).val():"|"+$(this).val());
+        }).val();
+        $('input[name="option[]"]').each(function(){
+          opciones+=(opciones.length<=0?$(this).val():"|"+$(this).val());
+        });
+        $('select[name="type[]"]').each(function(){
+          tipos+=(tipos.length<=0?$(this).val():"|"+$(this).val())
+        })
+        var create=courses.consult('./save',
+            {'course':course,'type':type,'f_inicio':f_inicio,
+             'f_fin':f_fin,'h_inicio':h_inicio,'h_fin':h_fin,
+             'preguntas':preguntas,'opciones':opciones,'tipos':tipos
+            },'POST');
+          create.done(function(d){ 
+            /*var msj='Operacion realizada con éxito';
+            var title='Mensaje!';
+            var error='';
+            if(d.oper==false){
+               msj='Error comuniquese con el Administrador';
+               title='Error!';
+               error=(d.error!='')?d.error:'';
+            }
+            courses.Modaloper('#modal_operacion',msj,title,error);*/
+          });
+      }); 
+    },
   }
   $(document).ready(function(){
     courses.launch();
--- resources/views/exams/create.blade.php
+++ resources/views/exams/create.blade.php
@@ -6,7 +6,64 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Examenes</div>
                 <div class="panel-body">
-                    
+                    <form class="data-render">
+                        <div class="form-group">
+                            <label for="course">Curso</label>
+                            <select name="course" id="course" class="form-control chosen-select"></select>
+                        </div>
+                        <div class="form-group">
+                            <label for="type-exam">Tipo de Examen</label>
+                            <select name="type-exam" id="type-exam" class="form-control chosen-select">
+                                <option value="p">Parcial</option>
+                                <option value="f">Final</option>
+                            </select>
+                        </div>    
+                        <div class="form-group">
+                            <label for="nombre">Fecha de Inicio: <span class="requerido">(*)</span></label>
+                            <div class="input-group date" data-provide="datepicker">
+                                <input type="text" class="form-control datepicker col-xs-12"
+                                    id="fecha_inicio" name="fecha_inicio" placeholder="dd/mm/yyyy">
+                                <div class="input-group-addon">
+                                    <span class="glyphicon glyphicon-th"></span>
+                               </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                                <label for="nombre">Hora de Inicio: <span class="requerido">(*)</span></label>
+                                <div class="input-group date" data-provide="datepicker">
+                                        <input type="text" class="form-control clockpicker col-xs-12"
+                                        id="hora_inicio" name="hora_inicio" placeholder="00:00:00">
+                                        <span class="input-group-addon">
+                                            <span class="glyphicon glyphicon-time"></span>
+                                        </span>
+                                </div>
+                          </div>
+                          <div class="form-group">
+                                <label for="nombre">Fecha de Finalizacion: <span class="requerido">(*)</span></label>
+                                <div class="input-group date" data-provide="datepicker">
+                                    <input type="text" class="form-control datepicker col-xs-12"
+                                    id="fecha_fin" name="fecha_fin" placeholder="dd/mm/yyyy">
+                                    <div class="input-group-addon">
+                                        <span class="glyphicon glyphicon-th"></span>
+                                    </div>
+                                </div>
+                          </div>
+                          <div class="form-group">
+                                <label for="nombre">Hora de Finalizacion: <span class="requerido">(*)</span></label>
+                                <div class="input-group date" data-provide="datepicker">
+                                        <input type="text" class="form-control clockpicker col-xs-12"
+                                        id="hora_final" name="hora_final" placeholder="00:00:00">
+                                        <span class="input-group-addon">
+                                            <span class="glyphicon glyphicon-time"></span>
+                                        </span>
+                                </div>
+                          </div>
+                    </form>
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+                            <button id="send-exam" class="btn btn-success">Enviar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
